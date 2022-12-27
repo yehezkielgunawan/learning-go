e6fb5048b5036a2d2138fb4ff23544e4739ccca9
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-27 20:12:13 +0700

    feat(main.go): :sparkles: belajar sampai tipe data

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("HAHAHA")
+	// Nambahin komentar
+	var firstName string = "john"
+	lastName := "wick"
+	lastName = "ethan"
+
+	_ = "belajar golang"
+
+	name := new(string)
+
+	var positiveNumber uint8 = 89
+	var negativeNumber = -123412345123
+	var decimalNumber = 2.62
+
+	var exist bool = true
+
+	var message = `Nama saya "John Pantau".
+	 Salam Kenal.
+	 Lagi Belajar Golang jancuk!`
+
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
+	fmt.Println(name)
+	fmt.Println(*name)
+	fmt.Printf("bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber)
+	fmt.Printf("bilangan desimal: %.3f\n", decimalNumber)
+	fmt.Printf("exist? %t \n", exist)
+	fmt.Println(message)
 }
