b77d703a366d5f82cfd08e7ab868360049a2d609
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2023-02-12 21:59:19 +0700

    feat(func-multiple-return): :sparkles: function with multiple return value

---
+++ func-multiple-return/func-multiple-return.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// func calculate(d float64) (float64, float64) {
+// 	var area = math.Pi * math.Pow(d/2, 2)
+
+// 	var circumference = math.Pi * d
+
+// 	return area, circumference
+// }
+
+// atau bisa pake cara ini
+
+func calculate(d float64) (area float64, circumference float64) {
+	area = math.Pi * math.Pow(d/2, 2)
+	circumference = math.Pi * d
+	return
+}
+
+func main() {
+	var diameter float64 = 15
+
+	var area, circumference = calculate(diameter)
+
+	fmt.Printf("luas lingkaran\t\t: %.2f\n", area)
+	fmt.Printf("keliling lingkaran\t: %.2f\n", circumference)
+}
---
+++ func-multiple-return/go.mod
@@ -0,0 +1,3 @@
+module func-multiple-return
+
+go 1.19
